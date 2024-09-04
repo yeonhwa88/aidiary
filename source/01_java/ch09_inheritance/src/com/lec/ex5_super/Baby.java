407818e37e4b9d5ab53ce74c97a2e87bf8094eb3
yeonhwa88 <gidarim0901@naver.com>
2024-09-04 18:04:46 +0900

    second

--- source/01_java/ch09_inheritance/src/com/lec/ex5_super/Baby.java
+++ source/01_java/ch09_inheritance/src/com/lec/ex5_super/Baby.java
@@ -9,7 +9,7 @@ public class Baby extends Person {
 		System.out.println("매개변수 없는 Baby 생성자 호출");
 	}
 	public Baby(String name, String character) {
-		super(name, character); // 부모클래스의 매개변수 있는 생성자 함수 . super() 메소드 반드시 첫번째 줄에 호출
+		super(name, character); // 부모클래스의 매개변수 있는 생성자 함수 호출 . super() 메소드 반드시 첫번째 줄에 호출
 		System.out.println("매배견수 있는 Baby 생성자 호출");
 	}
 	@Override
@@ -17,5 +17,4 @@ public class Baby extends Person {
 		System.out.print("♨ 응애응애 화난 아기");
 		super.intro(); // super.은 아무곳에나 있어도 가능
 	}
-	
 }
