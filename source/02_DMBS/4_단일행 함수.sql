d3f15fd0dfb3073fd0b916b8b701e09992f01434
yeonhwa88 <gidarim0901@naver.com>
2024-09-14 00:26:24 +0900

    24.09.13

---
+++ source/02_DMBS/4_단일행 함수.sql
@@ -0,0 +1,51 @@
+-- [IV] 단일행함수
+-- 함수 = 단일행함수 (4장) + 그룹함수 (5장)
+SELECT HIREDATE, TO_CHAR(HIREDATE, 'YYYY/MM/DD HH24:MI:SS')
+    FROM EMP; -- 단일행 적용
+    
+SELECT SUM(SAL), AVG(SAL)
+    FROM EMP; -- 그룹 함수 (INPUT 14행 -> OUTPUT 1행)
+SELECT DEPTNO, AVG(SAL) 
+    FROM EMP GROUP BY DEPTNO; -- INPUT N행 -> OUTPUT 1행
+
+-- ★ 단일행함수 = 숫자관련함수, 문자관련함수, 날짜관련함수, 형변환함수, NVL, ETC...
+-- 1. 숫자관련함수
+    -- DUAL 테이블 : 오라클에서 제공한 1행 1열짜리 DUMMY TABLE
+SELECT *
+    FROM DUAL;
+DESC DUAL;
+SELECT FLOOR (34.567)
+    FROM DUAL; -- 소수점에서 내림(버림)
+SELECT FLOOR (34.567*10)/10
+    FROM DUAL; -- 정수/정수 =>실수
+    
+SELECT TRUNC (34.567, 1) 
+    FROM DUAL; -- 소수점 한자리에서 내림
+SELECT TRUNC (34.567) 
+    FROM DUAL; -- 소수점 버림
+SELECT TRUNC (34.567, -1) 
+    FROM DUAL; -- 일의자리에서 내림
+    
+        -- EX. EMP에서 이름, 급여(십의자리에서 내림)
+        SELECT ENAME, SAL, TRUNC(SAL, -2)
+            FROM EMP;
+            
+SELECT CEIL (34.567) 
+    FROM DUAL; -- 소수점에서 올림 사용잘안함
+    
+SELECT ROUND (34.567)
+    FROM DUAL; -- 소수점에서 반올림
+SELECT RONUD (34.567 ,2)
+    FROM DUAL; -- 소수점 두자리에서 반올림
+SELECT ROUND (34.567, -1)
+    FROM DUAL; -- 일의자리에서 반올림
+    
+SELECT MOD(9, 2)
+    FROM DUAL; -- 나머지 
+    
+        -- 홀수년도에 입사한 사원의 모든 정보 출력
+        SELECT *
+            FROM EMP
+            WHERE MOD (TO_CHAR(HIREDATE, 'RR'), 2) =1;
+            
+    
