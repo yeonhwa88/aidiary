dd8ac46aab76cea127b3fec5a20cf781371cf885
yeonhaw88 <gidarim0901@naver.com>
2024-10-04 18:01:59 +0900

    24.10.04

---
+++ source/03_HTML-CSS/ch01/2.js
@@ -0,0 +1,7 @@
+﻿//2. js
+// 다른이름으로 저장 (utf-8인코딩 방법으로 저장)
+/* 동적인 부분(JacaScript) */
+name = prompt("이름은 ?", "홍길동"); // 취소를 클릭하면 'null' 리턴 // prompt (1:질문,2:디폴트값) 디폴트값은 안해도 가능
+if (name != "null" && name != "") {
+    document.write(name + "님 반갑습니다 <br>")
+}
