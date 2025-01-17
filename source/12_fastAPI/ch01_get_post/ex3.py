92243b965643b78f91b2ca4c091fad0517f4ae84
yeonhwa88 <gidarim0901@naver.com>
2025-01-17 12:33:16 +0900

    2025-01-17

--- source/12_fastAPI/ch01_get_post/ex3.py
+++ source/12_fastAPI/ch01_get_post/ex3.py
@@ -6,12 +6,38 @@ from fastapi import Request # html로 rendering 하기 위함 꼭
 from pydantic import BaseModel # form 데이터 자동 검증
 from fastapi import Form # post방식으로 들어온 form 데이터 받을 때
 
+class User(BaseModel):
+    name : str
+    id : str
+    pw : int
+
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates") # templates 지정 꼭
-app.mount("/static", StaticFiles(directory="static"), name="static") # static지정 꼭
+app.mount("/static", StaticFiles(directory="static"), name="static") # static 지정 꼭
 
 @app.get('/')
 
 @app.get('/html1')
 async def html1(request:Request):
     return templates.TemplateResponse('ex3-1.html', {'request':request})
+
+@app.get('/html2')
+async def html2(request:Request):
+    return templates.TemplateResponse("ex3-2.html",
+                                      {'request':request,
+                                        'name':'홍길동'})
+# @app.get('/html3')
+# async def html3(name:str, id:str, pw:int):
+#     return {"name":name, 'id':id, 'pw':pw}
+
+# pip install python-multipart
+
+@app.post('/html3')
+async def html3(user:User = Form()):
+    # return user.dict()
+    return {'name':user.name,
+            'id':user.id,
+            'pw':user.pw}
+# async def html3(name:str=Form(), id:str=Form(), pw:int=Form()):
+#     return {'name':name, 'id':id, 'pw':pw}
---
+++ source/12_fastAPI/ch02_fileupload/main.py
@@ -0,0 +1,78 @@
+# pip install fastapi
+# pip install uvicorn
+# pip install jinja2
+# pip install python-multipart
+from binascii import b2a_uu
+from os import access
+
+from fastapi import FastAPI, Request # 템플릿 사용
+from fastapi import File, UploadFile # 파일 첨부 시 사용
+from fastapi.staticfiles import StaticFiles # static (mout)지정
+from fastapi.templating import Jinja2Templates # templates 지정
+from starlette.responses import RedirectResponse # redirect 수행후 다른 요청경로 로 보낼때 사용
+from starlette.exceptions import HTTPException # 예외발생
+from fastapi.responses import FileResponse # 파일 다운로드
+
+import os
+import datetime
+
+app = FastAPI()
+app.mount('/static', StaticFiles(directory='static'), name='static')
+templates = Jinja2Templates(directory='templates')
+
+UPLOAD_FOLDER = './upload/'
+os.makedirs(UPLOAD_FOLDER,
+            exist_ok=True) # 디렉토리(폴더)가 없을 때만 생성
+
+def stamp2real(stamp):
+    return datetime.datetime.fromtimestamp(stamp)
+def info(filename):
+    create = stamp2real(os.path.getctime(filename)) # 언제 만들어졌는지
+    modify = stamp2real(os.path.getmtime(filename)) # 언제 수정됐는지
+    access = stamp2real(os.path.getatime(filename)) #
+    tempsize = os.path.getsize(filename)
+    size = ''
+    if tempsize < 1024*1024:
+        size = '{:.2f}KB'.format(tempsize / 1024)
+    else :
+        size = '{:.2f}MB'.format(tempsize/(1024*1024))
+    return  create, modify, access, size
+
+@app.get('/')
+@app.post('/')
+async def index(request:Request):
+    filelist = os.listdir(UPLOAD_FOLDER) # 업로드폴더에있는 파일 리스트
+    infos = []
+    for filename in filelist:
+        create, modify, access, size = info(UPLOAD_FOLDER + filename)
+        infos.append({
+            'name':filename,
+            'create':create,
+            'modify':modify,
+            'size':size
+        })
+    return templates.TemplateResponse("home.html",
+                                      {'request':request,
+                                       'infos':infos})
+
+@app.post('/upload')
+async def upload(request:Request, file:UploadFile=File()):
+    if file.filename: # 파일첨부 했음
+        # file_path = UPLOAD_FOLDER + file.filename
+        file_path = os.path.join(UPLOAD_FOLDER, file.filename)
+        with open(file_path, 'wb') as buffer :
+            buffer.write(await file.read()) # await : 비동기로 read() 함수 수행
+        create, modify, access, size = info(file_path)
+        fileinfo = {'ctime':create,
+                    'size':size}
+        return templates.TemplateResponse('check.html',
+                                          {'request':request,
+                                         'fileinfo':fileinfo})
+    else: # 파일첨부 안 함
+        return RedirectResponse(url='/',
+                                status_code=307)
+
+@app.get('/del/{filename}')
+async  def delete(filename:str):
+    os.remove(UPLOAD_FOLDER+filename)
+    return RedirectResponse('/')
