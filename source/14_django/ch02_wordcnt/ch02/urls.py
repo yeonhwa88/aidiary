0337b45b5a053cb890f45d3719112c7ce4dd9473
yeonhwa88 <gidarim0901@naver.com>
2025-02-11 11:32:58 +0900

    2025-02-11

--- source/14_django/ch02_wordcnt/ch02/urls.py
+++ source/14_django/ch02_wordcnt/ch02/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for ch01 project.
+URL configuration for ch02 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -20,5 +20,5 @@ from home import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", views.home,name="home"),
+    path("", views.index, name="index")
 ]
