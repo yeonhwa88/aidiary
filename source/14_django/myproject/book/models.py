4fc3b9a249c41c365cd5f79a05bc3a06d9f63ad5
yeonhwa88 <gidarim0901@naver.com>
2025-02-26 11:33:10 +0900

    2025-02-26

--- source/14_django/myproject/book/models.py
+++ source/14_django/myproject/book/models.py
@@ -3,6 +3,8 @@ from django import forms
 from django.core.validators import MinLengthValidator # 만들어진 함수 글자 수 에러
 from django.core.validators import MinValueValidator # 얼마 1원 이상
 from django.core.validators import MaxValueValidator # 얼마 100만원 까지
+from django.urls import reverse
+
 
 def min_length_3_validator(value):
     if len(value)<3:
@@ -16,7 +18,7 @@ class Book(models.Model):
     publisher = models.CharField(verbose_name=" 출판사", max_length=50, blank=True, null=True)
     publication_date = models.DateField(auto_now_add=True)
     sales = models.IntegerField(verbose_name="판매가",default=1000,
-                                validators=[MinValueValidator(1),
+                                validators=[MinValueValidator(0),
                                             MaxValueValidator(1000000)])
     ip = models.GenericIPAddressField(blank=True, null=True) # ip주소 자동저장
 
@@ -25,6 +27,9 @@ class Book(models.Model):
                                              self.author,
                                              self.sales,
                                              self.ip)
+    def get_absolute_url(self):
+        return reverse('book:list')
+        return reverse('book:list', args=[self.id])
     class Meta:
         ordering = ["-publication_date"] # 최근 입력된 순서부터 출력 orderby
         unique_together = (('title','author'), ) # title과 author가 두개가 똑같으면 저장이 안되고 하나만 중복되는건 저장이 가능
--- source/14_django/myproject/book/views.py
+++ source/14_django/myproject/book/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 
 from .forms import BookForm, BookModelForm
 from .models import Book
@@ -7,31 +7,57 @@ from django.views.generic import CreateView
 from django.views.generic import UpdateView
 from django.views.generic import DeleteView
 
-book_list = ListView.as_view(model=Book)
+book_list = ListView.as_view(model=Book) # 함수를 return
 
 # def book_list(request):
 #     return render(request,
 #                   "book/book_list.html",
 #                   {'book_list':Book.objects.all()})
 
-def book_new(request):
-    if request.method == "POST" :
-        #request.POST의 파라미터 값을 book 객체로 만든 후 save()
-        form = BookModelForm(request.POST, request.FILES) # FILES file이 넘어왔을때 사용
+book_new = CreateView.as_view(model=Book,
+                              fields=['title', 'author', 'publisher', 'sales'])
+
+# def book_new(request):
+#     if request.method == "POST" :
+#         #request.POST의 파라미터 값을 book 객체로 만든 후 save()
+#         form = BookModelForm(request.POST, request.FILES) # FILES file이 넘어왔을때 사용
+#         if form.is_valid():
+#             # book = Book(**form.cleaned_data) # 딕셔너리 형태, 입력받은 파라미터만 들어감
+#             book = form.save(commit=False) # 위와 동일
+#             book.ip = request.META['REMOTE_ADDR'] # 요청한 client의 ip
+#             book.save()
+#             # return redirect('book:list')
+#             return redirect(book) # book.get_absolute_url 자동호출
+#     else :
+#         # form = BookForm()
+#         form = BookModelForm()
+#     return render(request,
+#                   'book/book_form.html',
+#                   {'form':form})
+
+def book_edit(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    print('수정될 책 정보', book)
+    if request.method == 'POST':
+        # 파라미터정보를 form객체로 받아 유효성 체크 (1)성공 save (2)실패시 form태그페이지
+        form = BookModelForm(request.POST, instance=book)
         if form.is_valid():
-            book = Book(**form.cleaned_data) # 딕셔너리 형태, 입력받은 파라미터만 들어감
-            book.ip = request.META['REMOTE_ADDR'] # 요청한 client의 ip
+            book = form.save(commit=False)
+            # book.ip = request.META['REMOTE_ADDR'] # 수정한 ip 덮어 쓰기
             book.save()
-            return redirect('book:list')
-    else :
-        # form = BookForm()
-        form = BookModelForm()
-    return render(request,
-                  'book/book_form.html',
-                  {'form':form})
+            return redirect(book)
+    else:
+        form = BookModelForm(instance=book)
+        return render(request,
+                      'book/book_form.html',
+                      {'form':form})
 
-def book_edit(request):
-    pass
-
-def book_delete(request):
-    pass
+def book_delete(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return redirect(book)
+    else:
+        return render(request,
+               'book/book_confirm_delete.html',
+               {'object':book})
